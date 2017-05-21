b46870ec493ea46319bf41dce4c2359d7ca30e64
benji7425 <benji7425@outlook.com>
2017-05-21 03:00:35 +0100

    Update to use wrapper's command objects

--- app/index.js
+++ app/index.js
@@ -3,32 +3,40 @@ const Console = require("console");
 module.exports = (config) => {
 	config = config || require("./config.json");
 
-	this.onMessage = (bot, user, userID, channelID, message) => {
-		if (message.startsWith(config.roleAddCommand)) {
-			let roleName = message.split(" ")[1];
-			config.availableRoles.forEach((role) => {
-				if (role.name === roleName) {
-					bot.addToRole({
-						serverID: bot.channels[channelID].guild_id,
-						userID: userID,
-						roleID: role.id
-					}), (err, response) => { Console.error(err + " " + response); };
-				}
-			});
+	this.commands = [
+		{
+			command: config.roleAddCommand,
+			type: "startsWith",
+			action: (bot, user, userID, channelID, message) => {
+				let roleName = message.split(" ")[1];
+				config.availableRoles.forEach((role) => {
+					if (role.name === roleName) {
+						bot.addToRole({
+							serverID: bot.channels[channelID].guild_id,
+							userID: userID,
+							roleID: role.id
+						}), (err, response) => { Console.error(err + " " + response); };
+					}
+				});
+			}
+		},
+		{
+			command: config.roleRemoveCommand,
+			type: "startsWith",
+			action: (bot, user, userID, channelID, message) => {
+				let roleName = message.split(" ")[1];
+				config.availableRoles.forEach((role) => {
+					if (role.name === roleName) {
+						bot.removeFromRole({
+							serverID: bot.channels[channelID].guild_id,
+							userID: userID,
+							roleID: role.id
+						}), (err, response) => { Console.error(err + " " + response); };
+					}
+				});
+			}
 		}
-		else if (message.startsWith(config.roleRemoveCommand)) {
-			let roleName = message.split(" ")[1];
-			config.availableRoles.forEach((role) => {
-				if (role.name === roleName) {
-					bot.removeFromRole({
-						serverID: bot.channels[channelID].guild_id,
-						userID: userID,
-						roleID: role.id
-					}), (err, response) => { Console.error(err + " " + response); };
-				}
-			});
-		}
-	};
+	];
 
 	return this;
 };
