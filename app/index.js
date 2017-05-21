e8c0b61f37bbe441d5618bfcd89ca1f9b5c63d54
benji7425 <benji7425@outlook.com>
2017-05-21 02:22:48 +0100

    Add ability to pass config into module when loading with require

--- app/index.js
+++ app/index.js
@@ -1,11 +1,12 @@
-const Config = require("./config.json");
 const Console = require("console");
 
-module.exports = {
-	onMessage: (bot, user, userID, channelID, message) => {
-		if (message.startsWith(Config.roleAddCommand)) {
+module.exports = (config) => {
+	config = config || require("./config.json");
+
+	this.onMessage = (bot, user, userID, channelID, message) => {
+		if (message.startsWith(config.roleAddCommand)) {
 			let roleName = message.split(" ")[1];
-			Config.availableRoles.forEach((role) => {
+			config.availableRoles.forEach((role) => {
 				if (role.name === roleName) {
 					bot.addToRole({
 						serverID: bot.channels[channelID].guild_id,
@@ -15,9 +16,9 @@ module.exports = {
 				}
 			});
 		}
-		else if (message.startsWith(Config.roleRemoveCommand)) {
+		else if (message.startsWith(config.roleRemoveCommand)) {
 			let roleName = message.split(" ")[1];
-			Config.availableRoles.forEach((role) => {
+			config.availableRoles.forEach((role) => {
 				if (role.name === roleName) {
 					bot.removeFromRole({
 						serverID: bot.channels[channelID].guild_id,
@@ -27,5 +28,7 @@ module.exports = {
 				}
 			});
 		}
-	}
+	};
+
+	return this;
 };
