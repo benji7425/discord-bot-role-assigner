f44b48dc4ced2fd2851c1d7c8f4c3377e6f50366
benji7425 <benji7425@outlook.com>
2017-08-27 02:02:08 +0100

    Update to reflect code in my bot template

--- app/index.js
+++ app/index.js
@@ -9,43 +9,57 @@ const JsonFile = require("jsonfile");
 const DiscordUtil = require("discordjs-util");
 
 //global vars
-const SAVE_FILE = "./guilds.json";
+let writeFile = null;
 
 //when loaded with require() by an external script, this acts as a kind of "on ready" function
-module.exports = (client, config) => {
+module.exports = (client, config = null) => {
 	config = config || require("./config.json");
-	const data = FileSystem.existsSync(SAVE_FILE) ? JsonFile.readFileSync(SAVE_FILE) : {};
+	const guildsData = FileSystem.existsSync(config.generic.saveFile) ? fromJSON(JsonFile.readFileSync(config.generic.saveFile)) : {}; //read data from file, or generate new one if file doesn't exist
+	writeFile = () => JsonFile.writeFile(config.generic.saveFile, guildsData, err => { if (err) DiscordUtil.dateError("Error writing file", err); });
+	setInterval(() => writeFile(), config.generic.saveIntervalSec * 1000); //set interval to save data to file
 
-	client.on("message", (message) => {
-		if (message.channel.type !== "text" || !message.member)
-			return;
+	client.on("message", message => {
+		if (message.author.id !== client.user.id) { //check the bot isn't triggering itself
+			if (message.channel.type === "dm")
+				HandleMessage.dm(client, config, message);
+			else if (message.channel.type === "text" && message.member)
+				HandleMessage.text(client, config, message, guildsData);
+		}
+	});
+};
 
+const HandleMessage = {
+	dm: (client, config, message) => {
+		message.reply(config.generic.defaultDMResponse);
+	},
+	text: (client, config, message, guildsData) => {
 		const doCatch = e => { DiscordUtil.dateError(e); return e; },
 			doThen = m => message.reply(m || "An unknown error occured");
 
+		//handle admins invoking commands
 		if (message.content.startsWith(message.guild.me.toString()) //user is @mention-ing the bot
-			&& message.member.permissions.has("ADMINISTRATOR") //user has admin perms
-			&& message.member.id !== client.user.id) //isn't the bot accidentally triggering itself
+			&& message.member.permissions.has("ADMINISTRATOR")) //user has admin perms
 		{
-			const params = message.content.split(" "); //[ client.user.id, command, args ] expected
+			const params = message.content.split(" "); //split the message at the spaces
 
-			if (params.length < 2)
-				return;
-			
-			switch (params[1].toLowerCase()) {
-				case config.allowCommand:
-					RolePerms.allow(message.guild, data, params[2]).then(writeFile(data)).catch(doCatch).then(doThen);
+			//check which command was invoked
+			switch (params[1]) {
+				case config.commands.admin.version:
+					message.reply("v" + require("../package.json").version);
 					break;
-				case config.disallowCommand:
-					RolePerms.disallow(message.guild, data, params[2]).then(writeFile(data)).catch(doCatch).then(doThen);
+				case config.commands.admin.allowCommand:
+					RolePerms.allow(message.guild, guildsData, params[2]).then(writeFile(guildsData)).catch(doCatch).then(doThen);
+					break;
+				case config.commands.admin.disallowCommand:
+					RolePerms.disallow(message.guild, guildsData, params[2]).then(writeFile(guildsData)).catch(doCatch).then(doThen);
 					break;
 			}
 		}
-		else if (message.content.startsWith(config.joinCommand)) //user is trying to join a role
-			manageRole(message.guild, message.member, data, message.content.split(" ")[1], true).catch(doCatch).then(doThen);
-		else if (message.content.startsWith(config.leaveCommand)) //user is trying to leave a role
-			manageRole(message.guild, message.member, data, message.content.split(" ")[1], false).catch(doCatch).then(doThen);
-	});
+		else if (message.content.startsWith(config.commands.anyone.joinCommand)) //user is trying to join a role
+			manageRole(message.guild, message.member, guildsData, message.content.split(" ")[1], true).catch(doCatch).then(doThen);
+		else if (message.content.startsWith(config.commands.anyone.leaveCommand)) //user is trying to leave a role
+			manageRole(message.guild, message.member, guildsData, message.content.split(" ")[1], false).catch(doCatch).then(doThen);
+	}
 };
 
 /**
@@ -116,6 +130,6 @@ function normaliseRoleName(roleName) {
 	return (roleName || "").toLowerCase().replace(/ /g, "");
 }
 
-function writeFile(data) {
-	JsonFile.writeFile(SAVE_FILE, data, err => { if (err) DiscordUtil.dateError(err); });
+function fromJSON(json) {
+	return json;
 }
