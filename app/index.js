e4a26d8bfc462d62f5b634b2047550ce79b522af
benji7425 <benji7425@outlook.com>
2018-01-03 20:02:23 +0000

    Update to work with camo
    Also add eslint explicitly

--- app/index.js
+++ app/index.js
@@ -1,9 +1,7 @@
 const Core = require("../discord-bot-core");
 const GuildData = require("./models/guild-data.js");
 
-const token = require("../" + process.argv[2]).token,
-	dataFile = process.argv[3];
-
-const client = new Core.Client(token, dataFile, __dirname + "/commands", GuildData);
+// @ts-ignore
+const client = new Core.Client(require("../token.json"), __dirname + "/commands", GuildData);
 
 client.bootstrap();
