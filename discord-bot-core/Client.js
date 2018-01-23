86133a6f1fbd1fa6476ac0cb96bf1e72d21090b0
benji7425 <benji7425@outlook.com>
2018-01-23 12:50:42 +0000

    Manually update discord-bot-core subrepo
    subrepo:
      subdir:   "discord-bot-core"
      merged:   "109848a"
    upstream:
      origin:   "git@github.com:benji7425/discord-bot-core.git"
      branch:   "master"
      commit:   "109848a"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a7ee886"

--- discord-bot-core/Client.js
+++ discord-bot-core/Client.js
@@ -19,8 +19,7 @@ module.exports = class Client extends Discord.Client {
 	constructor(token, commandsDir, guildDataModel) {
 		super({
 			messageCacheMaxSize: 16,
-			messageCacheLifetime: 60,
-			messageSweepInterval: 480
+			disabledEvents: InternalConfig.disabledEvents
 		});
 
 		this._token = token;
@@ -100,6 +99,4 @@ function compactCollections() {
 	  and camo is designed to work with both NeDB and MongoDB, which is presumably why it doesn't alraedy exist */
 	for (let collectionName of Object.keys(neDB._collections))
 		neDB._collections[collectionName].persistence.compactDatafile();
-
-	Util.dateLog("Executed compaction on loaded NeDB collections");
 }
