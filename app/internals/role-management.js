91b1e90a4c37ad4b1996fd744adfea63999bfa85
benji7425 <benji7425@outlook.com>
2018-02-04 18:33:30 +0000

    Update error handling for when role can't be added

--- app/internals/role-management.js
+++ app/internals/role-management.js
@@ -8,9 +8,9 @@ function manageRole(guild, guildData, member, roleName, isJoining) {
 
         if (guildData.allowedRoles.includes(normalisedName)) {
             if (isJoining)
-                member.addRole(role).then(() => resolve("The role has been added")).catch(reject);
+                member.addRole(role).then(() => resolve("The role has been added")).catch(() => reject("Error adding role, ensure the bot has \"Manage roles\" permission and is higher in the list than the role you're trying to join."));
             else
-                member.removeRole(role).then(() => resolve("The role has been removed")).catch(reject);
+                member.removeRole(role).then(() => resolve("The role has been removed")).catch(() => reject("Error removing role, ensure the bot has \"Manage roles\" permission and is higher in the list than the role you're trying to join."));
         }
         else
             reject("You are not permitted to join/leave this role");
