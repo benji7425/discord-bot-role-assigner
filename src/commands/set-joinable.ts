b0d925e36ad7d5b0aee4c8f754dd5eb36916c1eb
benji7425 <benji7425@outlook.com>
2018-10-15 00:29:25 +0100

    Add set-joinable command

---
+++ src/commands/set-joinable.ts
@@ -0,0 +1,30 @@
+import { Message } from "../models/message";
+import { DisharmonyClient } from "disharmony";
+import Command, { PermissionLevel } from "disharmony/lib/commands/command";
+import { normaliseRole } from "../utilities";
+
+function invoke(params: string[], message: Message, client: DisharmonyClient)
+{
+    const name = params[0], normalised = normaliseRole(params[0]), joinable = params[1].toLowerCase() !== "false"
+
+    if (!message.guild.hasRole(normalised))
+        return Promise.reject(`Unable to find role ${name} in guild ${message.guild.name}`)
+
+    if (joinable && message.guild.joinableRoles.find(x => x === normalised))
+        return Promise.reject(`Role ${name} already joinable`)
+
+    if (joinable)
+        message.guild.joinableRoles.push(normalised)
+    else
+        message.guild.joinableRoles = message.guild.joinableRoles.filter(x => x !== normalised)
+        
+    return Promise.resolve(`Role ${name} is now ${!joinable ? "no longer" : ""} joinable`)
+}
+
+module.exports = new Command(
+    /*name*/            "set-joinable",
+    /*description*/     "Set a role as joinable and leavable",
+    /*syntax*/          "set-joinable <rolename> [true/false]",
+    /*permissionLevel*/ PermissionLevel.Admin,
+    /*invoke*/          invoke
+)
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Client } from "disharmony"
 import { Message } from "./models/message";
 
-let client = new Client("My bot", Message)
+let client = new Client("My bot", Message, require("./commands"))
 
 client.initialize("MzM3MDA2NTU4NjI0MDg4MDc0.DpGlHw.55m4LRAqzPmLNsolqFCgMtes0MA")
